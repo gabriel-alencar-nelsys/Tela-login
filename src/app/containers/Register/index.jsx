fd06160accfc882d4a549d62ac6c37d1b88c9d4b
João Gabriel <gabriel.alencar@nelsys.com.br>
2024-10-15 17:16:36 -0300

    Primeiro commit - adicionando minha aplicação

---
+++ src/app/containers/Register/index.jsx
@@ -0,0 +1,15 @@
+'use client'
+
+import {Title} from './styles'
+
+export const Login = () => {
+return(
+    <div>
+        <Title>
+            Login
+        </Title>
+    </div>
+)
+
+}
+
---
+++ src/app/containers/components/Button/index.jsx
@@ -0,0 +1,9 @@
+'use client'
+
+import { Button } from "./styles"
+
+export const Mybutton = () => {
+return(
+    <Button>Clique aqui</Button>
+)
+}
